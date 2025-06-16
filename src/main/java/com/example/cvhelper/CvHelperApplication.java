34f784a885ae49178f897f12a4a5ff3f377955c0
AlenaBaik <alenka.parasenka@gmail.com>
2025-06-16 21:53:36 +0200

    Spring Boot: базовая структура, контроллер и шаблон index.html

---
+++ src/main/java/com/example/cvhelper/CvHelperApplication.java
@@ -0,0 +1,11 @@
+package com.example.cvhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvHelperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CvHelperApplication.class, args);
+    }
+} 
